20a7f8fb7afb5a1b21b8e7a45d2f70f8b46ff70b
Sadie <sadie@Sadies-MacBook-Pro.local>
2022-04-01 14:29:27 -0700

    commit tree 350de877e878cedcdce216fbbbb717f9bf5165dc

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// code your solution here
+function saturdayFun(fun = "roller-skate") {
+    return `This Saturday, I want to ${fun}!`;
+}
+function mondayWork(work = "go to the office") {
+    return `This Monday, I will ${work}.`;
+}
+function wrapAdjective(hi = "special") {
+    console.log("*");
+    return `You are ${hi}!`;
+}
+
+
